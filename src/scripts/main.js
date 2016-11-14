1492163ee7063e94b3da883ceccb978683ca4b1e
Maxime Ghignet <maxime.ghignet@gmail.com>
2016-11-14 14:28:17 +0100

    adapt to the theme, use .quote and .subquotes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -27,3 +27,4 @@ bespoke.from('article', [
 // This is actually loaded from "bower_components" thanks to
 // debowerify: https://github.com/eugeneware/debowerify
 require('prism');
+require('prism/components/prism-git');
